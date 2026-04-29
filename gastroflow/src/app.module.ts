3963d31410627266da469db811cd6569a89caa95
lucianoagomez <lucianolg7@hotmail.com>
2026-04-28 21:26:41 -0300

    refactor: update order DTOs and status enum

--- gastroflow/src/app.module.ts
+++ gastroflow/src/app.module.ts
@@ -13,6 +13,7 @@ import { RestaurantModule } from './restaurants/restaurant.module';
 import { MenuModule } from './menu/menu.module';
 import { MailModule } from './mail/mail.module';
 import { ReservationsModule } from './reservations/reservations.module';
+import { ReservationsPaymentModule } from './reservations-payment/reservations-payment.module';
 import { RestaurantTablesModule } from './restaurant_tables/restaurant_tables.module';
 import { SubscriptionsModule } from './subscriptions/subscriptions.module';
 import { NotificationsModule } from './notification/notification.module';
@@ -38,6 +39,7 @@ import { OrderModule } from './orders/order.module';
     MailModule,
     RestaurantTablesModule,
     ReservationsModule,
+    ReservationsPaymentModule,
     SubscriptionsModule,
     NotificationsModule,
     OrderModule,
