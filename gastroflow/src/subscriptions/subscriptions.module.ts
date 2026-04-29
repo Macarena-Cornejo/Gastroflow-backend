f299c3342f7a1c21b91a8c2e3e8eb55af69c3f4c
lucianoagomez <lucianolg7@hotmail.com>
2026-04-28 22:32:22 -0300

    upgrade of suscription module

--- gastroflow/src/subscriptions/subscriptions.module.ts
+++ gastroflow/src/subscriptions/subscriptions.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { Subscription } from './entities/subscription.entity';
 import { Restaurant } from '../restaurants/entities/restaurant.entity';
 import { SubscriptionsService } from './subscriptions.service';
 import { SubscriptionsController } from './subscriptions.controller';
+import { MailService } from '../mail/mail.service';
 import { SubscriptionsCronService } from './subscriptions-cron.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Subscription, Restaurant])],
-  providers: [SubscriptionsService, SubscriptionsCronService],
+  providers: [SubscriptionsService, MailService, SubscriptionsCronService],
   controllers: [SubscriptionsController],
   exports: [SubscriptionsService, SubscriptionsCronService],
 })
