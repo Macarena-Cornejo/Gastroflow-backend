2f67383a6ddbfe0275be3112758243f918195f7a
yesidloaiza <losunayes@gmail.com>
2026-04-28 23:34:09 -0500

    feat: add subscription reminder cron with notification logs

--- gastroflow/src/subscriptions/subscriptions.module.ts
+++ gastroflow/src/subscriptions/subscriptions.module.ts
@@ -5,10 +5,14 @@ import { Restaurant } from '../restaurants/entities/restaurant.entity';
 import { SubscriptionsService } from './subscriptions.service';
 import { SubscriptionsController } from './subscriptions.controller';
 import { SubscriptionsCronService } from './subscriptions-cron.service';
+import { NotificationLog } from '../notification/entities/notification-log.entity';
 import { MailModule } from '../mail/mail.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Subscription, Restaurant]), MailModule],
+  imports: [
+    TypeOrmModule.forFeature([Subscription, Restaurant, NotificationLog]),
+    MailModule,
+  ],
   providers: [SubscriptionsService, SubscriptionsCronService],
   controllers: [SubscriptionsController],
   exports: [SubscriptionsService, SubscriptionsCronService],
