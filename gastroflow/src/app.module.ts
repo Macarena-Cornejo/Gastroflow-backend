09dbb6e41c1c15cceb11999dc0db3de4a76ff2ee
Nicolas Malvano <malvanonicolas@gmail.com>
2026-04-28 23:50:07 -0300

    Se implementa pagos de subscripciones con stripe

--- gastroflow/src/app.module.ts
+++ gastroflow/src/app.module.ts
@@ -19,6 +19,7 @@ import { SubscriptionsModule } from './subscriptions/subscriptions.module';
 import { NotificationsModule } from './notification/notification.module';
 import { OrderModule } from './orders/order.module';
 import { ScheduleModule } from '@nestjs/schedule';
+import { SubscriptionPaymentModule } from './subscriptions_payments/subscription_payment.module';
 
 @Module({
   imports: [
@@ -45,6 +46,7 @@ import { ScheduleModule } from '@nestjs/schedule';
     ReservationsModule,
     ReservationsPaymentModule,
     SubscriptionsModule,
+    SubscriptionPaymentModule,
     NotificationsModule,
     OrderModule,
     JwtModule.register({
